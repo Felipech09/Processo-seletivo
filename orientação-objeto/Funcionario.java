f61b73e657ad123623e547b9105a770debbe2bc0
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-09 23:59:08 -0300

    Update Funcionario.java
    correção de bugs

--- orientação-objeto/Funcionario.java
+++ orientação-objeto/Funcionario.java
@@ -4,37 +4,42 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Funcionario {
-  String nome;
-  String cargo;
-  LocalDate dataContrato;
-  double vendasMes;
+    String nome;
+    String cargo;
+    LocalDate dataContrato;
+    double vendasMes;
 
-  Funcionarios (String nome, String cargo, int mes, int ano) {
-    this.nome = nome;
-    this.cargo = cargo;
-    this.dataContrato = LocalDate.of(ano, mes, 1);
-  } 
+    // Construtor corrigido: nome da classe estava errado
+    public Funcionario(String nome, String cargo, int mes, int ano) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.dataContrato = LocalDate.of(ano, mes, 1);
+    } 
 
-  int anoServico(int mes, int ano) {
-    LocalDate atual = LocalDate.of(ano, mes, 1);
-    return Period.between(dataContrato, atual) .getYears();
-  }
+    // Método `anoServico` estava correto, mas a chamada para ele estava errada em outro método
+    int anosServico(int mes, int ano) {
+        LocalDate atual = LocalDate.of(ano, mes, 1);
+        return Period.between(dataContrato, atual).getYears();
+    }
 
-  double salario (int mes, int ano) {
-    int anos = anoervico(mes, ano);
-    if (cargo.equals("Secretário")) return 7000 + 1000 * anos;
-    if (cargo.equals("Vendedor")) return 12000 + 1800 * anos;
-    if (cargo.equals("Gerente")) return 20000 + 3000 * anos;
-    return 0;
-  }
+    // Método `salario` corrigido: `anoervico` estava com erro de digitação
+    double salario(int mes, int ano) {
+        int anos = anosServico(mes, ano);
+        if (cargo.equals("Secretário")) return 7000 + 1000 * anos;
+        if (cargo.equals("Vendedor")) return 12000 + 1800 * anos;
+        if (cargo.equals("Gerente")) return 20000 + 3000 * anos;
+        return 0;
+    }
 
-  double beneficio(int mes, int ano) {
-    if (cargo.equals("Secretário")) salario(mes, ano) * 0.2;
-    if (cargo.euals("Venddedor")) return vendasMes * 0.3;
-    return 0;
-  }
+    // Método `beneficio` corrigido: faltava `return`, `equals` e `Vendedor` estavam com erros de digitação
+    double beneficio(int mes, int ano) {
+        if (cargo.equals("Secretário")) return salario(mes, ano) * 0.2;
+        if (cargo.equals("Vendedor")) return vendasMes * 0.3;
+        return 0;
+    }
 
-  double totalRecebido(int mes, int ano) {
-    return salario(mes, ano) + beneficio(mes, ano);
-  }
+    // O método `totalRecebido` estava correto
+    double totalRecebido(int mes, int ano) {
+        return salario(mes, ano) + beneficio(mes, ano);
+    }
 }
